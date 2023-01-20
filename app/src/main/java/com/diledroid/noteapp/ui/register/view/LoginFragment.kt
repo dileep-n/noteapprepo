6efbad39e4210f0a36851c4e075d3c66dd15babc
dileep-n <dileepkaramel123@gmail.com>
2023-01-20 16:44:36 +0530

    otp auth completed

--- app/src/main/java/com/diledroid/noteapp/ui/register/view/LoginFragment.kt
+++ app/src/main/java/com/diledroid/noteapp/ui/register/view/LoginFragment.kt
@@ -1,23 +1,29 @@
 package com.diledroid.noteapp.ui.register.view
 
+
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.text.TextUtils
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.diledroid.noteapp.MainActivity
-
-
 import com.diledroid.noteapp.R
 import com.diledroid.noteapp.databinding.FragmentLoginBinding
-
 import com.diledroid.noteapp.ui.register.viewmodel.RegistrationViewModel
+import com.diledroid.noteapp.utils.KeycodeUtils
 import com.diledroid.noteapp.utils.ResultOf
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +41,14 @@ class LoginFragment : Fragment() {
     private var param2: String? = null
     private lateinit var binding: FragmentLoginBinding
     private lateinit var registerViewModel: RegistrationViewModel
+    // create instance of firebase auth
+    lateinit var auth: FirebaseAuth
+    private lateinit var bundle: Bundle
+    private lateinit var phone:String
+    // we will use this to match the sent otp from firebase
+    lateinit var storedVerificationId:String
+    lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,6 +68,8 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        auth=FirebaseAuth.getInstance()
+        bundle = Bundle()
         registerViewModel = (activity as RegisterActivity).fetchRegisterViewModel()
         //   initViewModel()
         observerLoadingProgress()
@@ -70,6 +86,57 @@ class LoginFragment : Fragment() {
                     .show()
             }
         }
+
+        binding.btnGo.setOnClickListener{
+            KeycodeUtils.hideKeyboard(context as Activity)
+            if (binding.etMobileNumber.text?.length != 10) {
+                Toast.makeText(requireContext(), "Please enter a valid mobile number", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+            sendVerificationCode("+91"+binding.etMobileNumber.text.toString())
+
+        }
+
+        // Callback function for Phone Auth
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            // This method is called when the verification is completed
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                registerViewModel.loading.postValue(false)
+                activity.let {
+                    val intent = Intent(it, MainActivity::class.java)
+                    startActivity(intent)
+                    it?.finish()
+                }
+            }
+
+            // Called when verification is failed add log statement to see the exception
+            override fun onVerificationFailed(e: FirebaseException) {
+                registerViewModel.loading.postValue(false)
+                Log.d("GFG" , "onVerificationFailed  $e")
+            }
+
+            // On code is sent by the firebase this method is called
+            // in here we start a new activity where user can enter the OTP
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                registerViewModel.loading.postValue(false)
+                Log.d("GFG","onCodeSent: $verificationId")
+                storedVerificationId = verificationId
+                resendToken = token
+                DetectingOtpDialogFragment
+                    .newInstance(phone,storedVerificationId)
+                    .show(requireActivity().supportFragmentManager, DetectingOtpDialogFragment.TAG)
+                // Start a new activity using intent
+                // also send the storedVerificationId using intent
+                // we will use this id to send the otp back to firebase
+
+            }
+        }
+
     }
 
 
@@ -120,6 +187,22 @@ class LoginFragment : Fragment() {
         })
 
 
+    }
+
+    // this method sends the verification code
+    // and starts the callback of verification
+    // which is implemented above in onCreate
+    private fun sendVerificationCode(number: String) {
+        phone = number
+        registerViewModel.loading.postValue(true)
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(number) // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(requireActivity()) // Activity (for callback binding)
+            .setCallbacks(callbacks) // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+        Log.d("GFG" , "Auth started")
     }
 
 
--- app/src/main/java/com/diledroid/noteapp/ui/register/view/SMSBroadcastReceiver.kt
+++ app/src/main/java/com/diledroid/noteapp/ui/register/view/SMSBroadcastReceiver.kt
@@ -1,4 +1,41 @@
 package com.diledroid.noteapp.ui.register.view
 
-class SMSBroadcastReceiver {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.diledroid.noteapp.utils.KeycodeUtils.getOtp
+import com.google.android.gms.auth.api.phone.SmsRetriever
+import com.google.android.gms.common.api.CommonStatusCodes
+import com.google.android.gms.common.api.Status
+
+class SMSBroadcastReceiver: BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (SmsRetriever.SMS_RETRIEVED_ACTION == intent.action) {
+            val extras = intent.extras
+            val status = extras!![SmsRetriever.EXTRA_STATUS] as Status?
+            when (status!!.statusCode) {
+                CommonStatusCodes.SUCCESS -> {
+                    val message = extras[SmsRetriever.EXTRA_SMS_MESSAGE] as String
+                    Log.e(TAG, "message: $message")
+                    val otp = getOtp(message)
+                    Log.e(TAG, "OTP: $otp")
+                    otpListener?.onOtpReceived(otp)
+                }
+                CommonStatusCodes.TIMEOUT -> {
+                }
+            }
+        }
+    }
+
+    companion object {
+        private val TAG = SMSBroadcastReceiver::class.java.simpleName
+
+        var otpListener: OtpListener? = null
+    }
+}
+
+interface OtpListener {
+    fun onOtpReceived(otp: String)
 }
